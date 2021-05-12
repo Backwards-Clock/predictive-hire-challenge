5afea295f0cd58da5e5e77dd4b709ecf1c8e148c
Lauren <laurenbendall@hotmail.com>
2021-05-12 12:52:51 +1000

    Add Home and Login page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "reactstrap";
+
+const Home = () => (
+  <Container>
+    <h1>Home</h1>
+    <p>
+      <a href="/login">Log In</a>.
+    </p>
+  </Container>
+);
+
+export default Home;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Container } from "reactstrap";
+
+const Login = () => {
+  return (
+    <Container>
+      <h1>Login</h1>
+    </Container>
+  );
+};
+
+export default Login;
