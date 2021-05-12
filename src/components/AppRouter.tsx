07a3a3f83226f1b60a48a8acc6863ff8192a64a0
Lauren <laurenbendall@hotmail.com>
2021-05-12 13:46:06 +1000

    Clean up unused import

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import PublicRoute from "./PublicRoute";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
